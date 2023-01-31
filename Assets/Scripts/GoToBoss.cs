d6ad16b19f45b0de848e69f16694062e97131079
0921ryang <0921ryang@gmail.com>
2023-01-31 14:23:36 +0100

    portal verbessern

--- Assets/Scripts/GoToBoss.cs
+++ Assets/Scripts/GoToBoss.cs
@@ -7,9 +7,12 @@ public class GoToBoss : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player")&&SceneManager.GetActiveScene().name=="SampleScene")
         {
             SceneManager.LoadScene(4);
+        }else if (other.CompareTag("Player")&&SceneManager.GetActiveScene().name=="Boss")
+        {
+            other.transform.position = new Vector3(0, 0, 0);
         }
     }
 }
